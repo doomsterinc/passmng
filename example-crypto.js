1b1a9fbe3f061c934fc19fa346decfb172708d88
doomsterinc <doomsterinc1@gmail.com>
2016-09-28 00:19:13 -0300

    tests crypto 5

--- example-crypto.js
+++ example-crypto.js
@@ -5,7 +5,7 @@ var secretMsg = "i hid the chips under the couch";
 var secretKey = "123abc";
 
 var encryptedMessage = crypto.AES.encrypt(secretMsg, secretKey);
-console.log(encryptedMessage);
+console.log("Message :" + encryptedMessage);
 var bytes = crypto.AES.decrypt(encryptedMessage, secretKey);
 var decryptedMessage = bytes.toString(crypto.enc.Utf8);
 console.log(decryptedMessage);
