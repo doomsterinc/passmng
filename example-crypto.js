af82206145b5144da039837deb5ef186ffde9961
doomsterinc <doomsterinc1@gmail.com>
2016-09-27 23:27:22 -0300

    tests crypto 2

--- example-crypto.js
+++ example-crypto.js
@@ -3,3 +3,5 @@ var crypto = require('crypto-js');
 var secretMsg = "i hid the chips under the couch";
 
 var secretKey = "123abc";
+
+var encryptedMessage = crypto.AES.encrypt(secretMsg, secretKey);
