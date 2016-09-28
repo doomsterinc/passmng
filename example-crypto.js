fcd1fb7d941c6cdc2b19376f9e285ac2979363d1
doomsterinc <doomsterinc1@gmail.com>
2016-09-28 00:27:51 -0300

    tests crypto 6

--- example-crypto.js
+++ example-crypto.js
@@ -1,11 +1,16 @@
 var crypto = require('crypto-js');
 
-var secretMsg = "i hid the chips under the couch";
-
+var secretMsg = {
+  name: 'Maicon',
+  secretName: 'Doom',
+  message:"i hid the chips under the couch"
+};
+var str = JSON.stringify(secretMsg);
+console.log(typeof secretMsg);
 var secretKey = "123abc";
 
-var encryptedMessage = crypto.AES.encrypt(secretMsg, secretKey);
-console.log("Message :" + encryptedMessage);
-var bytes = crypto.AES.decrypt(encryptedMessage, secretKey);
-var decryptedMessage = bytes.toString(crypto.enc.Utf8);
-console.log(decryptedMessage);
+//var encryptedMessage = crypto.AES.encrypt(secretMsg, secretKey);
+//console.log("Message :" + encryptedMessage);
+//var bytes = crypto.AES.decrypt(encryptedMessage, secretKey);
+//var decryptedMessage = bytes.toString(crypto.enc.Utf8);
+//console.log(decryptedMessage);
