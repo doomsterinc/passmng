fd4c787cf0b8941fe62bb962a0c45085fa367bee
doomsterinc <doomsterinc1@gmail.com>
2016-09-29 02:05:59 -0300

    try catch 3

--- exampleTryCatch.js
+++ exampleTryCatch.js
@@ -1,9 +1,21 @@
+// try {
+//   throw new Error("Unable to decrypt accounts.");
+// } catch (e) {
+//   console.log(e.message);
+// } finally {
+//   console.log("Finally block executed!");
+// }
+
+// console.log('try catch ended');
+
+function doWork(){
+  throw new Error("Unable to do work");
+}
+
 try {
-  throw new Error("Unable to decrypt accounts.");
+  doWork();
 } catch (e) {
   console.log(e.message);
 } finally {
   console.log("Finally block executed!");
 }
-
-console.log('try catch ended');
