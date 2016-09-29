e323790dc9e8dd6820efeca3a33b732fc71a5ddf
doomsterinc <doomsterinc1@gmail.com>
2016-09-29 02:00:13 -0300

    try catch 2

--- exampleTryCatch.js
+++ exampleTryCatch.js
@@ -1,7 +1,9 @@
 try {
-  throw new Error("Unable to decrypt accounts");
+  throw new Error("Unable to decrypt accounts.");
 } catch (e) {
   console.log(e.message);
+} finally {
+  console.log("Finally block executed!");
 }
 
 console.log('try catch ended');
